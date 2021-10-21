6b24d23b093691c00a61a13d8132e3dcc3353e6f
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-21 15:12:03 +0900

    .

---
+++ main/java/com/core/DB.java
@@ -0,0 +1,15 @@
+package com.core;
+
+import java.sql.*;
+import java.util.*;
+
+/**
+ * DB 연결 클래스
+ *
+ */
+public class DB {
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		
+		return null;
+	}
+}
