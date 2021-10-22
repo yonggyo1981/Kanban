504caa92c1b39c2efa25d1f2edfb45b09aed9be7
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-22 16:05:20 +0900

    .

--- main/java/com/controller/MemberController.java
+++ main/java/com/controller/MemberController.java
@@ -10,9 +10,105 @@ import java.io.IOException;
  */
 public class MemberController extends HttpServlet {
 	
+	private String httpMethod; // Http 요청 메서드, GET, POST
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-	}	
+		/** 요청 메서드 */
+		httpMethod = request.getMethod().toUpperCase();
+		if (httpMethod.equals("GET")) {
+			response.setContentType("text/html; charset=utf-8");
+		} else { // GET이 아닌 경우 -> 유입된 입력 양식 데이터 처
+			request.setCharacterEncoding("UTF-8");
+		}
+		
+		
+		String URI = request.getRequestURI();
+		String mode = URI.substring(URI.lastIndexOf("/") + 1);
+		switch(mode) {
+			case "join" : // 회원가입
+				joinController(request, response);
+				break;
+			case "info" : // 회원정보 수정
+				infoController(request, response);
+				break;
+			case "login" : // 로그인
+				loginController(request, response);
+				break;
+			case "findid" : // 아이디 찾기
+				findidController(request, response);
+				break;
+			case "findpw" : // 비밀번호 찾기
+				findpwController(request, response);
+				break;
+			default : // 없는 페이지 
+				RequestDispatcher rd = request.getRequestDispatcher("/error/404.jsp");
+				rd.forward(request, response);
+		}
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	/**
+	 * 회원 가입 /member/join
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void joinController(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	/**
+	 * 회원 정보 수정 /member/info
+	 * 
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void infoController(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	/**
+	 * 로그인 /member/login 
+	 * 
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void loginController(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	/**
+	 * 아이디 찾기 /member/findid
+	 * 
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void findidController(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+	
+	/**
+	 * 비밀번호 찾기
+	 * 
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void findpwController(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
 }
 
 
---
+++ main/webapp/error/404.jsp
@@ -0,0 +1,3 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+<h1>없는 페이지 입니다!!!</h1>
+<button type="button" onclick="history.back();">이전 페이지로 이동</button>
