8a26ad62e9ba8f39ab542387a2507ba4cdcf5b45
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-22 17:10:56 +0900

    .

---
+++ main/java/com/models/member/Member.java
@@ -0,0 +1,106 @@
+package com.models.member;
+
+import java.sql.*;
+
+/**
+ * Member bean 클래스 
+ *
+ */
+public class Member {
+	private int memNo; // 회원번호
+	private String memId; // 회원아이디
+	private String memPw; // 비밀번호
+	private String memPwHint; // 비밀번호 찾기 힌트
+	private String memNm; // 회원명
+	private String cellPhone; // 휴대전화번호
+	private String regDt; // 가입일시
+	
+	public Member() {}
+	
+	public Member(int memNo, String memId, String memPw, String memPwHint, String memNm, String cellPhone,
+			String regDt) {
+		super();
+		this.memNo = memNo;
+		this.memId = memId;
+		this.memPw = memPw;
+		this.memPwHint = memPwHint;
+		this.memNm = memNm;
+		this.cellPhone = cellPhone;
+		this.regDt = regDt;
+	}
+
+	public Member(ResultSet rs) throws SQLException {
+		this(
+			rs.getInt("memNo"),
+			rs.getString("memId"),
+			rs.getString("memPw"),
+			rs.getString("memPwHint"),
+			rs.getString("memNm"),
+			rs.getString("cellPhone"),
+			rs.getString("regDt")
+		);
+	}
+	
+	public int getMemNo() {
+		return memNo;
+	}
+	
+	public void setMemNo(int memNo) {
+		this.memNo = memNo;
+	}
+	
+	public String getMemId() {
+		return memId;
+	}
+	
+	public void setMemId(String memId) {
+		this.memId = memId;
+	}
+	
+	public String getMemPw() {
+		return memPw;
+	}
+	
+	public void setMemPw(String memPw) {
+		this.memPw = memPw;
+	}
+	
+	public String getMemPwHint() {
+		return memPwHint;
+	}
+	
+	public void setMemPwHint(String memPwHint) {
+		this.memPwHint = memPwHint;
+	}
+	
+	public String getMemNm() {
+		return memNm;
+	}
+	
+	public void setMemNm(String memNm) {
+		this.memNm = memNm;
+	}
+	
+	public String getCellPhone() {
+		return cellPhone;
+	}
+	
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+	
+	public String getRegDt() {
+		return regDt;
+	}
+	
+	public void setRegDt(String regDt) {
+		this.regDt = regDt;
+	}	
+}
+
+
+
+
+
+
+
