0b8008fa81fffde7166d663ccf1ee61b0d09477a
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-22 15:39:29 +0900

    .

---
+++ main/java/com/controller/MemberController.java
@@ -0,0 +1,18 @@
+package com.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+/**
+ *  /member/* 컨트롤러
+ *
+ */
+public class MemberController extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}	
+}
+
+
