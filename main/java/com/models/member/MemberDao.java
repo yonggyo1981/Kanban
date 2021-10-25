9607b8d0d914221e0ad32610b03c413312f19fa8
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-25 13:19:39 +0900

    .

--- main/java/com/models/member/MemberDao.java
+++ main/java/com/models/member/MemberDao.java
@@ -1,5 +1,8 @@
 package com.models.member;
 
+import java.util.*;
+import javax.servlet.http.*;
+
 /**
  * MemberDao 클래스 
  *
@@ -13,5 +16,42 @@ public class MemberDao {
 			instance = new MemberDao();
 		}
 		return instance;
+	}
+	
+	/**
+	 * 회원 가입 처리 
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public boolean join(HttpServletRequest request) {
+		
+		ArrayList<Map<String, String>> bindings = new ArrayList<>();
+		String sql = "INSERT INTO member (memId, memPw, memPwHint, memNm, cellPhone) VALUES (?,?,?,?,?)";
+		Map<String, String> map = new HashMap<>();
+		map.put("String", request.getParameter("memId"));
+		bindings.add(map);
+		
+		map = new HashMap<String, String>();
+		map.put("String", request.getParameter("memPw"));
+		bindings.add(map);
+		
+		
+		
+		return false;
+	}
+	
+	/**
+	 * SQL 바인데이터를 Map 형태로 지정
+	 * 
+	 * @param dataType
+	 * @param data
+	 * @return
+	 */
+	public Map<String, String> setBinding(String dataType, String data) {
+		Map<String, String> map = new HashMap<>();
+		map.put(dataType, data);
+		
+		return map;
 	}
 }
