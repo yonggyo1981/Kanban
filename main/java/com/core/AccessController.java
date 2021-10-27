3a17ce080813549286298a620f339d98d4f88af5
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-27 15:12:51 +0900

    .

--- main/java/com/core/AccessController.java
+++ main/java/com/core/AccessController.java
@@ -4,6 +4,8 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
 
+import com.models.member.*;
+
 /**
  * 페이지 접속 권한 체크 
  *
@@ -11,6 +13,7 @@ import java.io.*;
 public class AccessController {
 	
 	private static String requestURI;
+	private static boolean isLogin;
 	
 	/** 회원 전용 URI */
 	private static String[] memberOnlyURI = {"/member/info"};
@@ -27,6 +30,7 @@ public class AccessController {
 			if (request instanceof HttpServletRequest) {
 				HttpServletRequest req = (HttpServletRequest)request;
 				requestURI = req.getRequestURI();
+				isLogin = MemberDao.isLogin(request);
 				
 				// 비회원 전용 URI 체크 
 				checkGuestOnly();
@@ -48,7 +52,16 @@ public class AccessController {
 	 * @throws Exception
 	 */
 	private static void checkGuestOnly() throws Exception {
-		
+		/**
+		 * 로그인이 되어 있는 경우 -> 비회원 전용 페이지가 아닌 경우만 접근 가능 
+		 */
+		if (isLogin) {
+			for(String URI : guestOnlyURI) {
+				if (requestURI.indexOf(URI) != -1) { // 비회원 전용 페이지 접근한 경우 
+					throw new Exception("접근권한이 없습니다.");
+				}
+			}
+		}	
 	}
 }
 
