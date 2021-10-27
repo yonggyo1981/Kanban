e582b6c90b0694b17f2e37e0146d013a95daf18b
YONGGYO LEE <yonggyo1981@gmail.com>
2021-10-27 14:59:31 +0900

    .

--- main/java/com/core/AccessController.java
+++ main/java/com/core/AccessController.java
@@ -8,11 +8,27 @@ import java.io.*;
  *
  */
 public class AccessController {
+	
+	/** 회원 전용 URI */
+	private static String[] memberOnlyURI = {"/member/info"};
+	
+	/** 비회원 전용 URI */
+	private static String[] guestOnlyURI = {"/member/login", "/member/join"};
+	
 	/**
 	 * 페이지별 접속 체크 
 	 * @param request
 	 */
 	public static void init(ServletRequest request, ServletResponse response) {
-		
+		try {
+			
+		} catch (Exception e) {
+			Logger.log(e);
+			
+		}
 	}
 }
+
+
+
+
