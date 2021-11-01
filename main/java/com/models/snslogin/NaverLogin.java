3f2cc0a2fecd2bc0c5b51999de90864999ae2760
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-01 11:18:50 +0900

    .

--- main/java/com/models/snslogin/NaverLogin.java
+++ main/java/com/models/snslogin/NaverLogin.java
@@ -1,5 +1,8 @@
 package com.models.snslogin;
 
+import java.util.*;
+
+import com.core.Config;
 import com.models.member.Member;
 
 /**
@@ -18,6 +21,15 @@ public class NaverLogin extends SocialLogin {
 	public static NaverLogin getInstance() {
 		if (instance == null) {
 			instance = new NaverLogin();
+		}
+		
+		/** 네이버 로그인 설정 처리 */
+		
+		if (instance.clientId == null || instance.clientSecret == null || instance.callbackURL == null) {
+			HashMap<String, String> conf = (HashMap<String, String>)Config.getInstance().get("NaverLogin");
+			instance.clientId = conf.get("clientId");
+			instance.clientSecret = conf.get("secret");
+			instance.callbackURL = conf.get("callbackURL");
 		}
 		
 		return instance;
