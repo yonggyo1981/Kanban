a6ed6121fc94df7d01dd7f8ae03549f6d24f9413
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-01 11:40:37 +0900

    .

--- main/java/com/models/snslogin/NaverLogin.java
+++ main/java/com/models/snslogin/NaverLogin.java
@@ -1,8 +1,10 @@
 package com.models.snslogin;
 
 import java.util.*;
+import java.net.URLEncoder;
 
 import com.core.Config;
+import com.core.Logger;
 import com.models.member.Member;
 
 /**
@@ -25,10 +27,14 @@ public class NaverLogin extends SocialLogin {
 		
 		/** 네이버 로그인 설정 처리 */
 		if (instance.clientId == null || instance.clientSecret == null || instance.callbackURL == null) {
-			HashMap<String, String> conf = (HashMap<String, String>)Config.getInstance().get("NaverLogin");
-			instance.clientId = conf.get("clientId");
-			instance.clientSecret = conf.get("secret");
-			instance.callbackURL = conf.get("callbackURL");
+			try {
+				HashMap<String, String> conf = (HashMap<String, String>)Config.getInstance().get("NaverLogin");
+				instance.clientId = conf.get("clientId");
+				instance.clientSecret = conf.get("secret");
+				instance.callbackURL = URLEncoder.encode(conf.get("callbackURL"), "UTF-8");
+			} catch (Exception e) {
+				Logger.log(e);
+			}
 		}
 		
 		return instance;
@@ -36,8 +42,18 @@ public class NaverLogin extends SocialLogin {
 	
 	@Override
 	public String getCodeURL() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String state = String.valueOf(System.currentTimeMillis());
+		StringBuilder sb = new StringBuilder();
+		sb.append("https://nid.naver.com/oauth2.0/authorize?");
+		sb.append("response_type=code&client_id=");
+		sb.append(clientId);
+		sb.append("&redirect_uri=");
+		sb.append(callbackURL);
+		sb.append("&state=");
+		sb.append(state);
+		
+		return sb.toString();
 	}
 
 	@Override
