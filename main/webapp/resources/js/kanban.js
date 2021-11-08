ac514090ca3030452b6000c50285c6000bdc7f80
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-08 09:53:03 +0900

    .

--- main/webapp/resources/js/kanban.js
+++ main/webapp/resources/js/kanban.js
@@ -5,6 +5,18 @@
 window.addEventListener("DOMContentLoaded", function() {
 	const addWork = document.querySelector(".add_work");
 	addWork.addEventListener("click", function() {
-		layer.popup("../kanban/add", 500, 600);
+		layer.popup("../kanban/add", 500, 600, callbackAddPopup);
 	}, false);
 }, false);
+
+function callbackAddPopup()
+{
+	const addFile = document.querySelector(".add_file"); 
+	if (addFile) {
+		addFile.addEventListener("click", function() {
+			alert("클릭!");
+		}, false);
+	}
+}
+
+
