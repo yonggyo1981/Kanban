dcc1425417c32e256cc329d31821a18b13b01004
YONGGYO LEE <yonggyo1981@gmail.com>
2021-11-08 11:44:35 +0900

    .

--- main/java/com/controller/FileController.java
+++ main/java/com/controller/FileController.java
@@ -13,20 +13,62 @@ import java.util.*;
 public class FileController extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/views/error/404.jsp");
 		try {
-		String URI = request.getRequestURI();
-		String[] URIs = URI.split("/");
-		int idx = 0;
-		if (URIs[URIs.length - 1] != null) {
-			idx = Integer.valueOf(URIs[URIs.length - 1]);
-		}
-		String mode = URIs[URIs.length - 2];
-		} catch (ArrayIndexOutOfBoundsException e) {
+			String URI = request.getRequestURI();
+			String[] URIs = URI.split("/");
+			int idx = 0;
+			if (URIs[URIs.length - 1] != null) {
+				idx = Integer.valueOf(URIs[URIs.length - 1]);
+			}
+			String mode = URIs[URIs.length - 2];
+			switch(mode) {
+				case "download" :
+					downloadController(request, response, idx);
+					break;
+				case "delete" : 
+					deleteController(request, response, idx);
+					break;
+				default : 
+					rd.forward(request, response);
+			}
 			
+		} catch (ArrayIndexOutOfBoundsException e) {
+			rd.forward(request, response);
 		}
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
+	
+	/**
+	 * 다운로드 
+	 * 
+	 * @param request
+	 * @param response
+	 * @param idx - 파일 등록번호 
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void downloadController(HttpServletRequest request, HttpServletResponse response, int idx) throws ServletException, IOException {
+		
+	}
+	
+	/**
+	 * 파일 삭제 
+	 * 
+	 * @param request
+	 * @param response
+	 * @param idx 파일 등록번호
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void deleteController(HttpServletRequest request, HttpServletResponse response, int idx) throws ServletException, IOException {
+		
+	}
 }
+
+
+
+
